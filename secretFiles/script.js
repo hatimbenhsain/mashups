22a328346d6eeeb5a0d80a292ca9c4ca30c0e606
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2019-09-23 12:20:57 +0400

    Add files via upload

--- secretFiles/script.js
+++ secretFiles/script.js
@@ -1,6 +1,7 @@
 var userInput;
 var searchUrl="http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
 var contentUrl="https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=";
+var url2 = "https://en.wikipedia.org/w/api.php"; 
 var people={};
 var names=[];
 
@@ -9,22 +10,32 @@ function setup(){
 	userInput=select('#nameSearch');
 	userInput.changed(goWiki);
 	//goWiki();
-
+	var params = {
+	    action: "query",
+	    list: "search",
+	    srsearch: "Nelson Mandela",
+	    format: "json"
+	};
 	function goWiki(){
 		let term=userInput.value();
+		let params.srsearch=userInput.value();
 		//console.log(term);
 		let url=searchUrl + term;
+		url2 = url2 + "?origin=*";
+		Object.keys(params).forEach(function(key){url2 += "&" + key + "=" + params[key];});
 		loadJSON(url, gotData, 'jsonp');
 	}
 }
 
 function gotData(data){
-	console.log(data.query.search[0].title);
-	names.push(data.query.search[0].title);
-	people[names[names.length-1]]=new Person();
-	var title=data.query.search[0].title.replace(/\s+/g,'_');
-	var url=contentUrl+title;
-	loadJSON(url,gotContent,'jsonp');
+	fetch(url)
+    .then(function(response){return response.json();});
+	console.log(response.query.search[0].title);
+	// names.push(data.query.search[0].title);
+	// people[names[names.length-1]]=new Person();
+	// var title=data.query.search[0].title.replace(/\s+/g,'_');
+	// var url=contentUrl+title;
+	// loadJSON(url,gotContent,'jsonp');
 }
 
 function gotContent(data){
@@ -45,6 +56,7 @@ function gotContent(data){
 	console.log("http://"+p.imgUrl);
 	//console.log(temp);
 	createImg(p.imgUrl);
+
 }
 
 class Person{
